acfa3dd8dfbe5b5181a1f3d6514ed023eff593dd
Illia Volynets <volynets.i@hexide-digital.com>
2023-11-14 15:37:08 +0200

    feat: add getRoomsByReception and getReceptionByRoom methods for KaunasRoomDriver contract

--- src/Contracts/Drivers/Room/KaunasRoomDriver.php
+++ src/Contracts/Drivers/Room/KaunasRoomDriver.php
@@ -2,9 +2,12 @@
 
 namespace Hexide\EsvCore\Contracts\Drivers\Room;
 
+use Hexide\EsvCore\Classes\Response;
 use  Hexide\EsvCore\Contracts\Drivers\ReadOnlyDriver;
 
 interface KaunasRoomDriver extends ReadOnlyDriver
 {
+    public function getRoomsByReception($reception_cid): Response;
 
+    public function getReceptionByRoom($room_cid): Response;
 }
